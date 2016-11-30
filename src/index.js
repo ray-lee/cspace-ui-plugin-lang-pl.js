c4869cda547a841ada2030b4733045a9e9eb3d74
Ray Lee <rhlee@berkeley.edu>
2016-11-29 17:53:08 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default () => ({
+  messages: {
+    'loginForm.title': 'Ignsay Inyay',
+    'loginForm.prompt': 'Easeplay ignsay inyay otay ontinuecay.',
+    'loginForm.label.username': 'Emailyay',
+    'loginForm.label.password': 'Asswordpay',
+    'loginForm.label.submit': 'Ignsay inyay',
+    'about.title': 'Elcomeway otay ethay OllectioncayAcespay Emoday',
+  },
+});
